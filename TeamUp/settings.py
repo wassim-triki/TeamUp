ad485d7887359c3952de280b2a00f06c7ea03fc0
KamilChakroun <kamilchakroun7@gmail.com>
2025-10-26 14:15:12 +0100

    .

--- TeamUp/settings.py
+++ TeamUp/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'TeamUp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamupdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
